71fe215ce949c3552d094b10d5729e9edce73d20
adrian <59178218+Doctor-san@users.noreply.github.com>
2024-01-29 16:42:03 -0700

    Adding .env for secrecy...

--- server/db.js
+++ server/db.js
@@ -3,18 +3,12 @@ import 'dotenv/config';
 
 const { Pool } = pg;
 
-const user = process.env.PGUSER;
-const password = process.env.PGPASSWORD;
-const host = process.env.PGHOST;
-const port = process.env.PGPORT;
-const database = process.env.PGDATABASE;
-
 const pool = new Pool({
-    user: "adrian",
-    password: "password",
-    host: "localhost",
-    port: 5432,
-    database: "simpleblog"
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE
 });
 
 export default pool;
