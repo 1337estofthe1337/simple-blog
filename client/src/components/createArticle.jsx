20a1929690c10694ed1de9651a3a235069f38e4b
adrian <59178218+Doctor-san@users.noreply.github.com>
2024-01-30 12:20:15 -0700

    Added go back feature at create-article

--- client/src/components/createArticle.jsx
+++ client/src/components/createArticle.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 const CreateArticle = () => {
     
